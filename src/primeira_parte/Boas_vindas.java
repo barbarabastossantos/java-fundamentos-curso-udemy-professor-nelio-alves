1cd0d6c0ee48cf8db6eea662dec99b76c7ad7ae9
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-12-29 23:46:23 -0300

    Boas vindas

---
+++ src/primeira_parte/Boas_vindas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package primeira_parte;
+
+/**
+ *
+ * @author ADM
+ */
+public class Boas_vindas {
+    public static void main(String[] args) {
+        System.out.println("Boas vindas ao modulo java fundamentos, vamos travbalhar logica de programacao.");
+    }
+}
